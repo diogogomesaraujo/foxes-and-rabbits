7476dd8cd9d79d33f3354a89df5468e5e2a27b02
Rodrigo Araújo <rodrigogomesaraujo04@gmail.com>
2025-11-22 23:17:30 +0000

    indented propperly new

--- main.c
+++ main.c
@@ -11,35 +11,35 @@
 
 #define POSSIBLE_DIRECTIONS_LEN 4
 #define POSSIBLE_DIRECTIONS                                                    \
-  {(Direction){-1, 0}, (Direction){0, 1}, (Direction){1, 0},                   \
-   (Direction){0, -1}} // UP, RIGHT, LEFT DOWN
+    {(Direction){-1, 0}, (Direction){0, 1}, (Direction){1, 0},                 \
+     (Direction){0, -1}} // UP, RIGHT, LEFT DOWN
 #define NO_DIRECTION                                                           \
-  (Direction) { INT_MIN, INT_MIN }
+    (Direction) { INT_MIN, INT_MIN }
 #define IT_HAS_DIRECTION(d) NO_DIRECTION.x != d.x &&NO_DIRECTION.y != d.y
 
 typedef enum { Rabbit, Fox, Rock, None } CellID;
 
 typedef struct {
-  CellID id;
-  int age;
-  int gens_without_food;
+    CellID id;
+    int age;
+    int gens_without_food;
 } Cell;
 
 typedef struct {
-  int x;
-  int y;
+    int x;
+    int y;
 } Direction;
 
 typedef struct {
-  int gen_proc_rabbits;
-  int gen_proc_foxes;
-  int gen_food_foxes;
-  int n_gen;
-  int g;    // current gen
-  int r;    // rows
-  int c;    // cols
-  int n;    // starting num of enteties
-  Cell **m; // cell matrix
+    int gen_proc_rabbits;
+    int gen_proc_foxes;
+    int gen_food_foxes;
+    int n_gen;
+    int g;    // current gen
+    int r;    // rows
+    int c;    // cols
+    int n;    // starting num of enteties
+    Cell **m; // cell matrix
 } Environment;
 
 int input_file_to_env(char *file_path, Environment *env_buf);
@@ -52,29 +52,32 @@ Direction selecting_adjacent_cells(Environment e, int x, int y, Direction *d);
 Direction select_fox_direction(Environment e, int x, int y);
 Direction select_rabbit_direction(Environment e, int x, int y);
 
-Cell cell_from_id(CellID id) { return (Cell){id, STARTING_AGE, STARTING_GENS_WITHOUT_FOOD}; }
+Cell cell_from_id(CellID id) {
+    return (Cell){id, STARTING_AGE, STARTING_GENS_WITHOUT_FOOD};
+}
 
 Cell **allocate_empty_cell_matrix(int r, int c) {
-  Cell **m = (Cell **)malloc(sizeof(Cell *) * r);
-  for (int i = 0; i < r; i++) {
-    m[i] = (Cell *)malloc(sizeof(Cell) * c);
-    for (int j = 0; j < c; j++) {
-      m[i][j] = cell_from_id(None);
+    Cell **m = (Cell **)malloc(sizeof(Cell *) * r);
+    for (int i = 0; i < r; i++) {
+        m[i] = (Cell *)malloc(sizeof(Cell) * c);
+        for (int j = 0; j < c; j++) {
+            m[i][j] = cell_from_id(None);
+        }
     }
-  }
-  return m;
+    return m;
 }
 
 int destroy_cell_matrix(Cell **m, int r) {
-  for (int i = 0; i < r; i++) {
-    free(m[i]);
-  }
-  free(m);
-  return 0;
+    for (int i = 0; i < r; i++) {
+        free(m[i]);
+    }
+    free(m);
+    return 0;
 }
 
 int copy_cell_matrix(Cell **m, Cell **m_buf, int r, int c) {
-    for (int i = 0; i < r; i++) memcpy(m_buf[i], m[i], sizeof(Cell) * c);
+    for (int i = 0; i < r; i++)
+        memcpy(m_buf[i], m[i], sizeof(Cell) * c);
     return 0;
 }
 
@@ -85,309 +88,278 @@ int env_destroy(Environment e) {
     return 0;
 }
 
-int input_file_to_env(char* file_path, Environment *env_buf) {
-    FILE* file = fopen(file_path, "r");
+int input_file_to_env(char *file_path, Environment *env_buf) {
+    FILE *file = fopen(file_path, "r");
 
-  if (file == NULL) {
-    fprintf(stderr, "Couldn't open the file.\n");
-    return 1;
-  }
+    if (file == NULL) {
+        fprintf(stderr, "Couldn't open the file.\n");
+        return 1;
+    }
 
-  if (fscanf(file, "%d %d %d %d %d %d %d", &(*env_buf).gen_proc_rabbits,
-             &(*env_buf).gen_proc_foxes, &(*env_buf).gen_food_foxes,
-             &(*env_buf).n_gen, &(*env_buf).r, &(*env_buf).c,
-             &(*env_buf).n) == EOF) {
-    fprintf(stderr, "Couldn't parse the initial variables.\n");
-    return 1;
-  }
+    if (fscanf(file, "%d %d %d %d %d %d %d", &(*env_buf).gen_proc_rabbits,
+               &(*env_buf).gen_proc_foxes, &(*env_buf).gen_food_foxes,
+               &(*env_buf).n_gen, &(*env_buf).r, &(*env_buf).c,
+               &(*env_buf).n) == EOF) {
+        fprintf(stderr, "Couldn't parse the initial variables.\n");
+        return 1;
+    }
 
-  (*env_buf).g = 0;
-  (*env_buf).m = allocate_empty_cell_matrix((*env_buf).r, (*env_buf).c);
-  char *line_temp = malloc(sizeof(char) * STR_SIZE);
-  int x_temp, y_temp;
+    (*env_buf).g = 0;
+    (*env_buf).m = allocate_empty_cell_matrix((*env_buf).r, (*env_buf).c);
+    char *line_temp = malloc(sizeof(char) * STR_SIZE);
+    int x_temp, y_temp;
 
-  while (fscanf(file, "%s %d %d", line_temp, &x_temp, &y_temp) != EOF) {
-    if (strcmp(line_temp, "ROCK") == 0)
-      (*env_buf).m[x_temp][y_temp] = cell_from_id(Rock);
-    if (strcmp(line_temp, "RABBIT") == 0)
-      (*env_buf).m[x_temp][y_temp] = cell_from_id(Rabbit);
-    if (strcmp(line_temp, "FOX") == 0)
-      (*env_buf).m[x_temp][y_temp] = cell_from_id(Fox);
-  }
+    while (fscanf(file, "%s %d %d", line_temp, &x_temp, &y_temp) != EOF) {
+        if (strcmp(line_temp, "ROCK") == 0)
+            (*env_buf).m[x_temp][y_temp] = cell_from_id(Rock);
+        if (strcmp(line_temp, "RABBIT") == 0)
+            (*env_buf).m[x_temp][y_temp] = cell_from_id(Rabbit);
+        if (strcmp(line_temp, "FOX") == 0)
+            (*env_buf).m[x_temp][y_temp] = cell_from_id(Fox);
+    }
 
-  fclose(file);
-  return 0;
+    fclose(file);
+    return 0;
 }
 
-Direction selecting_adjacent_cells(Environment e, int x, int y, Direction d[POSSIBLE_DIRECTIONS_LEN]) {
-  int d_len = 0;
-  for (int i = 0; i < 4; i++)
-    if (IT_HAS_DIRECTION(d[i]))
-      d_len++;
+Direction selecting_adjacent_cells(Environment e, int x, int y,
+                                   Direction d[POSSIBLE_DIRECTIONS_LEN]) {
+    int d_len = 0;
+    for (int i = 0; i < 4; i++)
+        if (IT_HAS_DIRECTION(d[i]))
+            d_len++;
 
-  if (d_len == 0)
-    return NO_DIRECTION;
+    if (d_len == 0)
+        return NO_DIRECTION;
 
-  int choosing_value = (e.g + x + y) % d_len;
+    int choosing_value = (e.g + x + y) % d_len;
 
-  int count = 0, cv_count = 0;
-  while (count < POSSIBLE_DIRECTIONS_LEN) {
-    if (cv_count == choosing_value && IT_HAS_DIRECTION(d[count]))
-      break;
-    if (IT_HAS_DIRECTION(d[count]))
-      cv_count++;
+    int count = 0, cv_count = 0;
+    while (count < POSSIBLE_DIRECTIONS_LEN) {
+        if (cv_count == choosing_value && IT_HAS_DIRECTION(d[count]))
+            break;
+        if (IT_HAS_DIRECTION(d[count]))
+            cv_count++;
 
-    count++;
-  }
+        count++;
+    }
 
-  return d[count];
+    return d[count];
 }
 
 Direction select_rabbit_direction(Environment e, int x, int y) {
-  assert(e.m[x][y].id == Rabbit);
+    assert(e.m[x][y].id == Rabbit);
 
-  Direction d[POSSIBLE_DIRECTIONS_LEN] = POSSIBLE_DIRECTIONS;
-  if (x == 0)
-    d[0] = NO_DIRECTION;
-  if (y == e.c - 1)
-    d[1] = NO_DIRECTION;
-  if (x == e.r - 1)
-    d[2] = NO_DIRECTION;
-  if (y == 0)
-    d[3] = NO_DIRECTION;
+    Direction d[POSSIBLE_DIRECTIONS_LEN] = POSSIBLE_DIRECTIONS;
+    if (x == 0)
+        d[0] = NO_DIRECTION;
+    if (y == e.c - 1)
+        d[1] = NO_DIRECTION;
+    if (x == e.r - 1)
+        d[2] = NO_DIRECTION;
+    if (y == 0)
+        d[3] = NO_DIRECTION;
 
-  for (int i = 0; i < POSSIBLE_DIRECTIONS_LEN; i++)
-    if (IT_HAS_DIRECTION(d[i]) && e.m[x + d[i].x][y + d[i].y].id != None)
-      d[i] = NO_DIRECTION;
+    for (int i = 0; i < POSSIBLE_DIRECTIONS_LEN; i++)
+        if (IT_HAS_DIRECTION(d[i]) && e.m[x + d[i].x][y + d[i].y].id != None)
+            d[i] = NO_DIRECTION;
 
-  return selecting_adjacent_cells(e, x, y, d);
+    return selecting_adjacent_cells(e, x, y, d);
 }
 
 Direction select_fox_direction(Environment e, int x, int y) {
-  assert(e.m[x][y].id == Fox);
+    assert(e.m[x][y].id == Fox);
 
-  // logic for rabbits
-  Direction rabbit_dirs[POSSIBLE_DIRECTIONS_LEN] = POSSIBLE_DIRECTIONS;
-  if (x == 0)
-    rabbit_dirs[0] = NO_DIRECTION;
-  if (y == e.c - 1)
-    rabbit_dirs[1] = NO_DIRECTION;
-  if (x == e.r - 1)
-    rabbit_dirs[2] = NO_DIRECTION;
-  if (y == 0)
-    rabbit_dirs[3] = NO_DIRECTION;
+    // logic for rabbits
+    Direction rabbit_dirs[POSSIBLE_DIRECTIONS_LEN] = POSSIBLE_DIRECTIONS;
+    if (x == 0)
+        rabbit_dirs[0] = NO_DIRECTION;
+    if (y == e.c - 1)
+        rabbit_dirs[1] = NO_DIRECTION;
+    if (x == e.r - 1)
+        rabbit_dirs[2] = NO_DIRECTION;
+    if (y == 0)
+        rabbit_dirs[3] = NO_DIRECTION;
 
-  for (int i = 0; i < POSSIBLE_DIRECTIONS_LEN; i++)
-    if (IT_HAS_DIRECTION(rabbit_dirs[i]) &&
-        e.m[x + rabbit_dirs[i].x][y + rabbit_dirs[i].y].id != Rabbit)
-      rabbit_dirs[i] = NO_DIRECTION;
+    for (int i = 0; i < POSSIBLE_DIRECTIONS_LEN; i++)
+        if (IT_HAS_DIRECTION(rabbit_dirs[i]) &&
+            e.m[x + rabbit_dirs[i].x][y + rabbit_dirs[i].y].id != Rabbit)
+            rabbit_dirs[i] = NO_DIRECTION;
 
-  Direction rd = selecting_adjacent_cells(e, x, y, rabbit_dirs);
-  if (IT_HAS_DIRECTION(rd))
-    return rd;
+    Direction rd = selecting_adjacent_cells(e, x, y, rabbit_dirs);
+    if (IT_HAS_DIRECTION(rd))
+        return rd;
 
-  // logic for empty
-  Direction empty_dirs[POSSIBLE_DIRECTIONS_LEN] = POSSIBLE_DIRECTIONS;
-  if (x == 0)
-    empty_dirs[0] = NO_DIRECTION;
-  if (y == e.c - 1)
-    empty_dirs[1] = NO_DIRECTION;
-  if (x == e.r - 1)
-    empty_dirs[2] = NO_DIRECTION;
-  if (y == 0)
-    empty_dirs[3] = NO_DIRECTION;
+    // logic for empty
+    Direction empty_dirs[POSSIBLE_DIRECTIONS_LEN] = POSSIBLE_DIRECTIONS;
+    if (x == 0)
+        empty_dirs[0] = NO_DIRECTION;
+    if (y == e.c - 1)
+        empty_dirs[1] = NO_DIRECTION;
+    if (x == e.r - 1)
+        empty_dirs[2] = NO_DIRECTION;
+    if (y == 0)
+        empty_dirs[3] = NO_DIRECTION;
 
-  for (int i = 0; i < POSSIBLE_DIRECTIONS_LEN; i++)
-    if (IT_HAS_DIRECTION(empty_dirs[i]) &&
-        e.m[x + empty_dirs[i].x][y + empty_dirs[i].y].id != None)
-      empty_dirs[i] = NO_DIRECTION;
+    for (int i = 0; i < POSSIBLE_DIRECTIONS_LEN; i++)
+        if (IT_HAS_DIRECTION(empty_dirs[i]) &&
+            e.m[x + empty_dirs[i].x][y + empty_dirs[i].y].id != None)
+            empty_dirs[i] = NO_DIRECTION;
 
-  Direction ed = selecting_adjacent_cells(e, x, y, empty_dirs);
-  return ed;
+    Direction ed = selecting_adjacent_cells(e, x, y, empty_dirs);
+    return ed;
 }
 
 int single_rabbit_move(Environment e, Cell **copy, int x, int y) {
-  Direction d = select_rabbit_direction(e, x, y);
-  if (IT_HAS_DIRECTION(d)) {
-    switch (copy[x + d.x][y + d.y].id) {
+    Direction d = select_rabbit_direction(e, x, y);
+    if (IT_HAS_DIRECTION(d)) {
+        switch (copy[x + d.x][y + d.y].id) {
         case Rabbit:
-            // if has greater proc age or same proc age and has less --> replace
             if (e.m[x][y].age > copy[x + d.x][y + d.y].age) {
                 copy[x + d.x][y + d.y] = e.m[x][y];
             }
             break;
-
-    case None:
-      // else --> take position and increase (hunger)
-      copy[x + d.x][y + d.y] = e.m[x][y];
-      break;
-
-    default:
-      printf("single_rabbit_move entered unexpected case \n");
-      return 1;
+        case None:
+            copy[x + d.x][y + d.y] = e.m[x][y];
+            break;
+        default:
+            printf("single_rabbit_move entered unexpected case \n");
+            return 1;
+        }
+        copy[x][y] = cell_from_id(None);
+        if (e.m[x][y].age >= e.gen_proc_rabbits) {
+            copy[x + d.x][y + d.y].age = STARTING_AGE - 1; // spaggethi logic
+            copy[x][y] = cell_from_id(Rabbit);
+        }
+        copy[x + d.x][y + d.y].age++;
     }
-    copy[x][y] = cell_from_id(None);
-    if (e.m[x][y].age >= e.gen_proc_rabbits) {
-      copy[x + d.x][y + d.y].age = STARTING_AGE - 1; // spaggethi logic
-      copy[x][y] = cell_from_id(Rabbit);
+    else {
+        copy[x][y] = e.m[x][y];
+        copy[x][y].age++;
     }
-
-    copy[x + d.x][y + d.y].age++;
-  }
-
-  // if does not have direction
-  else {
-      copy[x][y] = e.m[x][y];
-      copy[x][y].age++; // increase age --> update copy matrix
-  }
-
-  return 0;
+    return 0;
 }
 
 int single_fox_move(Environment e, Cell **copy, int x, int y) {
-  Direction d = select_fox_direction(e, x, y);
-
-  if (IT_HAS_DIRECTION(d)) {
-    switch (copy[x + d.x][y + d.y].id) {
-
-    case Rabbit:
-      // eat --> replace and loose hunger
-      copy[x + d.x][y + d.y] = e.m[x][y];
-      copy[x + d.x][y + d.y].gens_without_food = STARTING_GENS_WITHOUT_FOOD;
-
-      break;
-
-    case Fox:
-      // if has greater proc age or same proc age and has less --> replace
-      if (e.m[x][y].age > copy[x + d.x][y + d.y].age ||
-          (e.m[x][y].age == copy[x + d.x][y + d.y].age &&
-           e.m[x][y].gens_without_food <
-               copy[x + d.x][y + d.y].gens_without_food)) {
-        copy[x + d.x][y + d.y] = e.m[x][y]; // replace
-      }
-      // else --> keep the one already occupying the Cell (the one that is
-      // moving disappears)
-
-      copy[x + d.x][y + d.y].gens_without_food++; // increase gens_without_food (hunger)
-      break;
-
-    case None:
-      // if about to die --> die and don't move
-      if (e.m[x][y].gens_without_food >= e.gen_food_foxes) {
-          copy[x][y] = cell_from_id(None);
-        return 0;
-      }
-
-      // else --> take position and increase (hunger)
-      else {
-        copy[x + d.x][y + d.y] = e.m[x][y];
-        copy[x + d.x][y + d.y].gens_without_food++;
-      }
-      break;
-
-    default:
-      printf("single_fox_move entered unexpected case \n");
-      return 1;
+    Direction d = select_fox_direction(e, x, y);
+    if (IT_HAS_DIRECTION(d)) {
+        switch (copy[x + d.x][y + d.y].id) {
+        case Rabbit:
+            copy[x + d.x][y + d.y] = e.m[x][y];
+            copy[x + d.x][y + d.y].gens_without_food = STARTING_GENS_WITHOUT_FOOD;
+            break;
+        case Fox:
+            if (e.m[x][y].age > copy[x + d.x][y + d.y].age ||
+                (e.m[x][y].age == copy[x + d.x][y + d.y].age &&
+                 e.m[x][y].gens_without_food <
+                     copy[x + d.x][y + d.y].gens_without_food)) {
+                copy[x + d.x][y + d.y] = e.m[x][y]; // replace
+            }
+            copy[x + d.x][y + d.y]
+                .gens_without_food++;
+            break;
+        case None:
+            if (e.m[x][y].gens_without_food >= e.gen_food_foxes) {
+                copy[x][y] = cell_from_id(None);
+                return 0;
+            }
+            else {
+                copy[x + d.x][y + d.y] = e.m[x][y];
+                copy[x + d.x][y + d.y].gens_without_food++;
+            }
+            break;
+        default:
+            printf("single_fox_move entered unexpected case \n");
+            return 1;
+        }
+        copy[x][y] = cell_from_id(None);
+        if (e.m[x][y].age >= e.gen_proc_foxes) {
+            copy[x + d.x][y + d.y].age = STARTING_AGE - 1; // spaggethi logic
+            copy[x][y] = cell_from_id(Fox);
+        }
+        copy[x + d.x][y + d.y].age++;
     }
-
-    // copy[x + d.x][y + d.y] is already correctly updated at this step
-    // and erase always prev pos
-
-    copy[x][y] = cell_from_id(None);
-
-    // if can procriate --> leave fox in place --> both procriation ages go to 0
-    if (e.m[x][y].age >= e.gen_proc_foxes) {
-        copy[x + d.x][y + d.y].age = STARTING_AGE - 1; // spaggethi logic
-      copy[x][y] = cell_from_id(Fox);
+    else {
+        if (e.m[x][y].gens_without_food >= e.gen_food_foxes) {
+            copy[x][y] = cell_from_id(None);
+        } else {
+            copy[x][y] = e.m[x][y];
+            copy[x][y].age++;
+        }
     }
-
-    copy[x + d.x][y + d.y].age++; // increase age --> update copy matrix
-  }
-
-  // if does not have direction
-  else {
-    // age or die logic for fox that stayed in-place
-    if (e.m[x][y].gens_without_food >= e.gen_food_foxes) {
-      copy[x][y] = cell_from_id(None); // die
-    } else {
-      copy[x][y] = e.m[x][y];
-      copy[x][y].age++; // increase age --> update copy matrix
-    }
-  }
-
-  return 0;
+    return 0;
 }
 
 int next_gen(Environment *e_buf) {
-  Cell **new_m = allocate_empty_cell_matrix((*e_buf).r, (*e_buf).c);
-  copy_cell_matrix((*e_buf).m, new_m, (*e_buf).r, (*e_buf).c);
+    Cell **new_m = allocate_empty_cell_matrix((*e_buf).r, (*e_buf).c);
+    copy_cell_matrix((*e_buf).m, new_m, (*e_buf).r, (*e_buf).c);
 
-  // rabbit
-  for (int i = 0; i < (*e_buf).r; i++) {
-      for (int j = 0; j < (*e_buf).c; j++) {
-          if ((*e_buf).m[i][j].id == Rabbit) {
-              single_rabbit_move((*e_buf), new_m, i, j);
-          }
-      }
-  }
+    // rabbit
+    for (int i = 0; i < (*e_buf).r; i++) {
+        for (int j = 0; j < (*e_buf).c; j++) {
+            if ((*e_buf).m[i][j].id == Rabbit) {
+                single_rabbit_move((*e_buf), new_m, i, j);
+            }
+        }
+    }
 
-  copy_cell_matrix(new_m, (*e_buf).m, (*e_buf).r, (*e_buf).c);
+    copy_cell_matrix(new_m, (*e_buf).m, (*e_buf).r, (*e_buf).c);
 
-  //fox
-  for (int i = 0; i < (*e_buf).r; i++) {
-      for (int j = 0; j < (*e_buf).c; j++) {
-          if ((*e_buf).m[i][j].id == Fox) {
-              single_fox_move((*e_buf), new_m, i, j);
-          }
-      }
-  }
+    // fox
+    for (int i = 0; i < (*e_buf).r; i++) {
+        for (int j = 0; j < (*e_buf).c; j++) {
+            if ((*e_buf).m[i][j].id == Fox) {
+                single_fox_move((*e_buf), new_m, i, j);
+            }
+        }
+    }
 
-  destroy_cell_matrix((*e_buf).m, (*e_buf).r);
-  (*e_buf).m = new_m;
+    destroy_cell_matrix((*e_buf).m, (*e_buf).r);
+    (*e_buf).m = new_m;
 
-  (*e_buf).g++;
+    (*e_buf).g++;
 
-  // check fox died against original matrix pos (if rabbit --> survive, else -->
-  // die)
+    // check fox died against original matrix pos (if rabbit --> survive, else -->
+    // die)
 
-  return 0;
+    return 0;
 }
 
 void print_environment(Environment e) {
-  printf("Gen: %d ", e.g);
-  for (int i = 0; i < e.r; i++) {
-    printf("\n");
-    for (int j = 0; j < e.c; j++) {
-      switch (e.m[i][j].id) {
-      case Rabbit:
-        printf("R ");
-        break;
-      case Rock:
-        printf("* ");
-        break;
-      case Fox:
-        printf("F ");
-        break;
-      default:
-        printf("_ ");
-        break;
-      }
+    printf("Gen: %d ", e.g);
+    for (int i = 0; i < e.r; i++) {
+        printf("\n");
+        for (int j = 0; j < e.c; j++) {
+            switch (e.m[i][j].id) {
+            case Rabbit:
+                printf("R ");
+                break;
+            case Rock:
+                printf("* ");
+                break;
+            case Fox:
+                printf("F ");
+                break;
+            default:
+                printf("_ ");
+                break;
+            }
+        }
     }
-  }
-  printf("\n\n");
+    printf("\n\n");
 }
 
 int main(int argc, char **argv) {
-  Environment e;
-  if (argc != 2)
-    return 1;
-  if (input_file_to_env(argv[1], &e) == 1) {
-    return 1;
-  }
-  print_environment(e);
-  for (int i = 0; i < e.n_gen; i++) {
-      next_gen(&e);
-      print_environment(e);
-  }
-  return 0;
+    Environment e;
+    if (argc != 2)
+        return 1;
+    if (input_file_to_env(argv[1], &e) == 1) {
+        return 1;
+    }
+    print_environment(e);
+    for (int i = 0; i < e.n_gen; i++) {
+        next_gen(&e);
+        print_environment(e);
+    }
+    return 0;
 }
